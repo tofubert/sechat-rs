71b581250ae614f67c7d02796de57142c68de1b2
Patrick Hoffmann <p.hoffmann@berlin.de>
2024-08-28 16:15:38 +0200

    make NcMessage a tuple struct

--- src/backend/nc_message.rs
+++ src/backend/nc_message.rs
@@ -2,66 +2,60 @@ use crate::backend::nc_request::NCReqDataMessage;
 use chrono::prelude::*;
 
 #[derive(Debug)]
-pub struct NCMessage {
-    data: NCReqDataMessage,
-    message: String,
-}
+pub struct NCMessage(NCReqDataMessage);
 
 impl From<NCReqDataMessage> for NCMessage {
     fn from(data: NCReqDataMessage) -> Self {
-        NCMessage {
-            message: data.message.clone(),
-            data,
-        }
+        NCMessage(data)
     }
 }
 
 impl NCMessage {
     pub fn get_time_str(&self) -> String {
         let time: DateTime<Local> =
-            DateTime::from(DateTime::<Utc>::from_timestamp(self.data.timestamp, 0).unwrap());
+            DateTime::from(DateTime::<Utc>::from_timestamp(self.0.timestamp, 0).unwrap());
         time.format("%H:%M").to_string()
     }
 
     pub fn get_name(&self) -> String {
-        self.data.actorDisplayName.clone()
+        self.0.actorDisplayName.clone()
     }
 
-    pub fn get_message(&self) -> String {
-        self.message.clone()
+    pub fn get_message(&self) -> &str {
+        &self.0.message
     }
 
     pub fn get_reactions_str(&self) -> String {
         let mut reactions = String::new();
-        for (icon, number) in &self.data.reactions {
+        for (icon, number) in &self.0.reactions {
             reactions = reactions + "('" + icon + "' times " + &number.to_string() + "), ";
         }
         reactions
     }
 
     pub fn get_id(&self) -> i32 {
-        self.data.id
+        self.0.id
     }
 
     pub fn to_data(&self) -> NCReqDataMessage {
-        self.data.clone()
+        self.0.clone()
     }
 
     pub fn is_comment(&self) -> bool {
-        self.data.messageType == "comment"
+        self.0.messageType == "comment"
     }
 
     pub fn is_comment_deleted(&self) -> bool {
-        self.data.messageType == "comment_deleted"
+        self.0.messageType == "comment_deleted"
     }
 
     pub fn is_system(&self) -> bool {
-        self.data.messageType == "system"
+        self.0.messageType == "system"
     }
 
     pub fn is_edit_note(&self) -> bool {
         if self.is_system() {
-            self.data.systemMessage == "message_edited"
+            self.0.systemMessage == "message_edited"
         } else {
             false
         }
@@ -69,17 +63,17 @@ impl NCMessage {
 
     pub fn is_reaction(&self) -> bool {
         if self.is_system() {
-            self.data.systemMessage == "reaction"
+            self.0.systemMessage == "reaction"
         } else {
             false
         }
     }
 
     pub fn is_command(&self) -> bool {
-        self.data.messageType == "command"
+        self.0.messageType == "command"
     }
 
     pub fn has_reactions(&self) -> bool {
-        !self.data.reactions.is_empty()
+        !self.0.reactions.is_empty()
     }
 }
