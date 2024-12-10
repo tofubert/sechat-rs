053d6baeca84599aa6117c14a07114acf2116c52
Michel von Czettritz und Neuhaus <mvc@emlix.com>
2024-12-10 10:04:05 +0100

    fix typo in theme

--- src/config/theme.rs
+++ src/config/theme.rs
@@ -8,20 +8,20 @@ use std::str::FromStr;
 #[serde_as]
 #[derive(Serialize, Deserialize, Debug)]
 pub struct Theme {
-    /// Default Backround
+    /// Default Background
     #[serde_as(as = "DisplayFromStr")]
-    pub backround: Color,
+    pub background: Color,
     /// Default Text Colour
     #[serde_as(as = "DisplayFromStr")]
     pub foreground: Color,
     #[serde_as(as = "DisplayFromStr")]
-    pub backround_highlight: Color,
+    pub background_highlight: Color,
     #[serde_as(as = "DisplayFromStr")]
     pub foreground_highlight: Color,
 
-    /// Backround for unread message highlight
+    /// background for unread message highlight
     #[serde_as(as = "DisplayFromStr")]
-    pub backround_unread_message: Color,
+    pub background_unread_message: Color,
     /// Foreground for unread message highlight
     #[serde_as(as = "DisplayFromStr")]
     pub foreground_unread_message: Color,
@@ -35,7 +35,7 @@ pub struct Theme {
     pub foreground_titlebar: Color,
     /// Text Colour for titlebar contents
     #[serde_as(as = "DisplayFromStr")]
-    pub backround_important_titlebar: Color,
+    pub background_important_titlebar: Color,
     /// Text Colour for titlebar contents
     #[serde_as(as = "DisplayFromStr")]
     pub foreground_important_titlebar: Color,
@@ -52,43 +52,48 @@ pub struct Theme {
 
 impl Theme {
     pub fn default_style(&self) -> Style {
-        Style::new().fg(self.foreground).bg(self.backround)
+        Style::new().fg(self.foreground).bg(self.background)
     }
     pub fn default_highlight_style(&self) -> Style {
         Style::new()
             .fg(self.foreground_highlight)
-            .bg(self.backround_highlight)
+            .bg(self.background_highlight)
     }
     pub fn user_away_style(&self) -> Style {
-        Style::new().fg(self.user_away).bg(self.backround)
+        Style::new().fg(self.user_away).bg(self.background)
     }
     pub fn user_dnd_style(&self) -> Style {
-        Style::new().fg(self.user_dnd).bg(self.backround)
+        Style::new().fg(self.user_dnd).bg(self.background)
     }
     pub fn user_offline_style(&self) -> Style {
-        Style::new().fg(self.user_offline).bg(self.backround)
+        Style::new().fg(self.user_offline).bg(self.background)
     }
     pub fn user_online_style(&self) -> Style {
-        Style::new().fg(self.user_online).bg(self.backround)
+        Style::new().fg(self.user_online).bg(self.background)
     }
     pub fn unread_message_style(&self) -> Style {
         Style::new()
             .fg(self.foreground_unread_message)
-            .bg(self.backround_unread_message)
+            .bg(self.background_unread_message)
     }
 
     pub fn table_header_style(&self) -> Style {
-        Style::new().bold().fg(self.table_header).bg(self.backround)
+        Style::new()
+            .bold()
+            .fg(self.table_header)
+            .bg(self.background)
     }
 
     pub fn title_status_style(&self) -> Style {
-        Style::new().bg(self.backround).fg(self.foreground_titlebar)
+        Style::new()
+            .bg(self.background)
+            .fg(self.foreground_titlebar)
     }
 
     pub fn title_important_style(&self) -> Style {
         Style::new()
             .bold()
-            .bg(self.backround_important_titlebar)
+            .bg(self.background_important_titlebar)
             .fg(self.foreground_important_titlebar)
     }
 }
@@ -96,19 +101,19 @@ impl Theme {
 impl Default for Theme {
     fn default() -> Self {
         Self {
-            backround: Color::DarkGray,
+            background: Color::DarkGray,
             foreground: Color::White,
-            backround_highlight: Color::Gray,
+            background_highlight: Color::Gray,
             foreground_highlight: Color::White,
             user_away: Color::Blue,
             user_dnd: Color::Red,
             user_offline: Color::Gray,
             user_online: Color::Green,
-            backround_unread_message: Color::from_str("#6e6a86").unwrap(),
+            background_unread_message: Color::from_str("#6e6a86").unwrap(),
             foreground_unread_message: Color::from_str("#e0def4").unwrap(),
             table_header: Color::Blue,
             foreground_titlebar: Color::White,
-            backround_important_titlebar: Color::Red,
+            background_important_titlebar: Color::Red,
             foreground_important_titlebar: Color::White,
         }
     }
