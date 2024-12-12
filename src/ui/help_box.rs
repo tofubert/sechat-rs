290fbd4e8be54d7b4973c5d56814672a4dd477cc
Michel von Czettritz und Neuhaus <mvc@emlix.com>
2024-12-12 18:06:02 +0100

    add test for help_box

--- src/ui/help_box.rs
+++ src/ui/help_box.rs
@@ -72,3 +72,50 @@ impl Widget for &HelpBox {
         );
     }
 }
+
+#[cfg(test)]
+mod tests {
+
+    use crate::backend::nc_request::NCReqDataParticipants;
+    use crate::config::init;
+    use backend::TestBackend;
+
+    use super::*;
+
+    #[test]
+    fn render_users() {
+        let dir = tempfile::tempdir().unwrap();
+
+        std::env::set_var("HOME", dir.path().as_os_str());
+        let config = init("./test/").unwrap();
+
+        let backend = TestBackend::new(40, 10);
+        let mut terminal = Terminal::new(backend).unwrap();
+        let help_box = HelpBox::new(&config);
+
+        let mut dummy_user = NCReqDataParticipants::default();
+        dummy_user.displayName = "Butz".to_string();
+
+        terminal
+            .draw(|frame| help_box.render_area(frame, Rect::new(0, 0, 40, 10)))
+            .unwrap();
+
+        let mut expected = Buffer::with_lines([
+            "Key   Name                 Behavior     ",
+            "q     quit                 enter the qui",
+            "o     open                 enter the cha",
+            "u     users sidebar        Toggle whethe",
+            "?     help                 enter this he",
+            "m     mark as read         mark current ",
+            "(e|i) edit                 enter the edi",
+            "ESC   leave Mode           leave help, o",
+            "Enter send/select          Send Message,",
+            "                                        ",
+        ]);
+        expected.set_style(Rect::new(0, 0, 40, 10), config.theme.default_style());
+
+        expected.set_style(Rect::new(0, 0, 40, 1), config.theme.table_header_style());
+
+        terminal.backend().assert_buffer(&expected);
+    }
+}
